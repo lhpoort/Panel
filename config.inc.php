69afa29745052045088afccb5a022cb1d748d201
Larry Poort <larry.poort@anan6.com>
2018-08-29 12:16:30 +0200

    Initialize project

---
+++ config.inc.php
@@ -0,0 +1,5 @@
+<?php
+define("PBX_GATEWAY_UUID", "aaecc015-d58f-4783-bc9b-b7d0e1066fdf");
+define("PBX_STAT_GROUPS", '{"aftersales": [211,212,213,214,215,216], "receptie": [200,201], "sales": [231,232,233,234,235,236], "algemeen": []}');
+define("PBX_CACHE", 60);
+?>
